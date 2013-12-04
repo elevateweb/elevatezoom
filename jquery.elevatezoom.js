573ca20640b48beca208022c638a66b5a2c683aa
krusty <krusty.ar@gmail.com>
2013-12-04 20:41:17 -0200

    Fix lens proportion calculation
    The condition was incorrectly checking for the width of the zoom window instead of the height, causing the lens to be equal height with the non-zoomed image when the actual viewport was smaller

--- jquery.elevatezoom.js
+++ jquery.elevatezoom.js
@@ -189,7 +189,7 @@ if ( typeof Object.create !== 'function' ) {
 
 					// adjust images less than the window height
 
-					if(self.nzHeight < self.options.zoomWindowWidth/self.widthRatio){
+					if(self.nzHeight < self.options.zoomWindowHeight/self.heightRatio){
 						lensHeight = self.nzHeight;              
 					}
 					else{
