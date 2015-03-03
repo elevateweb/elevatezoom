887e36c7b08b9d6a698869ef7dc6b9a6e6af7196
Kyle Macey <kyle@wholelattelove.com>
2015-03-03 18:24:25 -0500

    bump status

--- jquery.elevatezoom.js
+++ jquery.elevatezoom.js
@@ -1,5 +1,5 @@
 /*
- *	jQuery elevateZoom 3.0.8
+ *	jQuery elevateZoom 3.1.0
  *	Demo's and documentation:
  *	www.elevateweb.co.uk/image-zoom
  *
@@ -10,18 +10,6 @@
  *	http://en.wikipedia.org/wiki/MIT_License
  *	http://en.wikipedia.org/wiki/GNU_General_Public_License
  *
-
-/*
- *	jQuery elevateZoom 3.0.3
- *	Demo's and documentation:
- *	www.elevateweb.co.uk/image-zoom
- *
- *	Copyright (c) 2012 Andrew Eades
- *	www.elevateweb.co.uk
- *
- *	Dual licensed under the GPL and MIT licenses.
- *	http://en.wikipedia.org/wiki/MIT_License
- *	http://en.wikipedia.org/wiki/GNU_General_Public_License
  */
 
 
