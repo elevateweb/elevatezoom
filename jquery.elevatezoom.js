934c7f7911817945ab078c88a83fab3aad9a9766
Jefferson Rafael Kozerski <jeff.drumgod@gmail.com>
2015-09-23 17:26:34 -0300

    Change file header information
    removed duplicate info about version

--- jquery.elevatezoom.js
+++ jquery.elevatezoom.js
@@ -9,21 +9,7 @@
  *	Dual licensed under the GPL and MIT licenses.
  *	http://en.wikipedia.org/wiki/MIT_License
  *	http://en.wikipedia.org/wiki/GNU_General_Public_License
- *
-
-/*
- *	jQuery elevateZoom 3.0.3
- *	Demo's and documentation:
- *	www.elevateweb.co.uk/image-zoom
- *
- *	Copyright (c) 2012 Andrew Eades
- *	www.elevateweb.co.uk
- *
- *	Dual licensed under the GPL and MIT licenses.
- *	http://en.wikipedia.org/wiki/MIT_License
- *	http://en.wikipedia.org/wiki/GNU_General_Public_License
  */
-
 
 if ( typeof Object.create !== 'function' ) {
 	Object.create = function( obj ) {
